58fcb7c0dc99139b6aba81f73271da3675f74ba6
Stavroula Bakogianni <stauroulabak@gmail.com>
2024-08-03 23:22:37 +0300

    Fix Owner object creation in createOwner method of OwnerService

--- src/main/java/gr/technico/technikon/services/OwnerService.java
+++ src/main/java/gr/technico/technikon/services/OwnerService.java
@@ -5,6 +5,7 @@ import gr.technico.technikon.model.Owner;
 import gr.technico.technikon.repositories.OwnerRepository;
 
 public class OwnerService implements OwnerServiceInterface {
+
     private final OwnerRepository ownerRepository;
 
     public OwnerService(OwnerRepository ownerRepository) {
@@ -13,7 +14,15 @@ public class OwnerService implements OwnerServiceInterface {
 
     @Override
     public void createOwner(String vat, String name, String surname, String address, String phoneNumber, String email, String username, String password) throws CustomException {
-        Owner owner = new Owner(null, vat, name, surname, address, phoneNumber, email, username, password);
+        Owner owner = new Owner();
+        owner.setVat(vat);
+        owner.setAddress("Kavala");
+        owner.setName(name);
+        owner.setSurname(surname);
+        owner.setPhoneNumber(phoneNumber);
+        owner.setUsername(username);
+        owner.setPassword(password);
+        owner.setEmail(email);
         
         try {
             ownerRepository.save(owner);
