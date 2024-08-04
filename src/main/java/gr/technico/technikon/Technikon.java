7fb74219635656531037fb93075946ee0e45bcee
Stavroula Bakogianni <stauroulabak@gmail.com>
2024-08-04 23:20:25 +0300

    Execute owners csv import on app start

--- src/main/java/gr/technico/technikon/Technikon.java
+++ src/main/java/gr/technico/technikon/Technikon.java
@@ -3,15 +3,21 @@ package gr.technico.technikon;
 import gr.technico.technikon.services.OwnerService;
 import gr.technico.technikon.repositories.OwnerRepository;
 import gr.technico.technikon.jpa.JpaUtil;
+import gr.technico.technikon.services.importfiles.OwnerCSVImporter;
 import gr.technico.technikon.ui.MainMenuUI;
+import java.io.IOException;
 
 public class Technikon {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
+
+        OwnerCSVImporter ownerCSVImporter = new OwnerCSVImporter();
+        ownerCSVImporter.importFile("csv_files/owners.csv");
+
         OwnerRepository ownerRepository = new OwnerRepository(JpaUtil.getEntityManager());
         OwnerService ownerService = new OwnerService(ownerRepository);
         MainMenuUI mainMenuUI = new MainMenuUI(ownerService);
-
+ 
         mainMenuUI.run();
     }
 }
