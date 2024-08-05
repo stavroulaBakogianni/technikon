f7ced2782c9c6076684cecc3dbe68608987024c9
Stavroula Bakogianni <stauroulabak@gmail.com>
2024-08-06 02:20:17 +0300

    Introduce RepairRepository structure

---
+++ src/main/java/gr/technico/technikon/repositories/RepairRepository.java
@@ -0,0 +1,55 @@
+package gr.technico.technikon.repositories;
+
+import gr.technico.technikon.jpa.JpaUtil;
+import gr.technico.technikon.model.Owner;
+import gr.technico.technikon.model.Repair;
+import java.util.List;
+import java.util.Optional;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+public class RepairRepository implements Repository<Repair, Long> {
+
+    private final EntityManager entityManager;
+
+    public RepairRepository(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public Optional<Repair> findById(Long id) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public List<Repair> findAll() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Optional<Repair> save(Repair repair) {
+        try {
+            JpaUtil.beginTransaction();
+            entityManager.persist(repair);
+            JpaUtil.commitTransaction();
+            return Optional.of(repair);
+        } catch (Exception e) {
+            JpaUtil.rollbackTransaction();
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public boolean deleteById(Long id) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    private Class<Repair> getEntityClass() {
+        return Repair.class;
+    }
+
+    private String getEntityClassName() {
+        return Repair.class.getName();
+    }
+
+}
