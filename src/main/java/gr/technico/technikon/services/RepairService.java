663fd874af0762be17d2b73da68c02adce467fb4
arisp84 <papazisisaris@gmail.com>
2024-08-05 23:07:10 +0300

    4x update methods: -updGeneral -updCostDates -updAcceptance -updComplete

--- src/main/java/gr/technico/technikon/services/RepairService.java
+++ src/main/java/gr/technico/technikon/services/RepairService.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package gr.technico.technikon.services;
 
 import gr.technico.technikon.exceptions.CustomException;
@@ -9,20 +5,23 @@ import gr.technico.technikon.model.Owner;
 import gr.technico.technikon.model.Property;
 import gr.technico.technikon.model.Repair;
 import gr.technico.technikon.model.RepairType;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
-/**
- *
- * @author Admin
- */
 public interface RepairService {
-
+    
     Repair createRepair(RepairType repairType, String shortDescription,
-//            LocalDateTime submissionDate, 
-            String description,
-//            RepairStatus repairStatus,
-            Owner owner,
-            Property property);
+            String description, Owner owner, Property property);
+    
+    void updGeneral(Long id, RepairType repairType, String shortDescription,
+            String description); 
+    
+    public void updCostDates(Long id, BigDecimal proposedCost, LocalDateTime proposedStartDate, LocalDateTime proposedEndDateTime);
+
+    public void updAcceptance(Long id, int response);
+    
+    public void  updComplete(Long id);
 
     Long saveRepair(Repair repair) throws CustomException;
 
--- src/main/java/gr/technico/technikon/services/RepairServiceImpl.java
+++ src/main/java/gr/technico/technikon/services/RepairServiceImpl.java
@@ -7,6 +7,7 @@ import gr.technico.technikon.model.Repair;
 import gr.technico.technikon.model.RepairStatus;
 import gr.technico.technikon.model.RepairType;
 import gr.technico.technikon.repositories.RepairRepository;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -18,12 +19,10 @@ public class RepairServiceImpl implements RepairService {
     public RepairServiceImpl(RepairRepository repairRepository) {
         this.repairRepository = repairRepository;
     }
+
     @Override
     public Repair createRepair(RepairType repairType, String shortDescription,
-//            LocalDateTime submissionDate, 
-            String description,
-//            RepairStatus repairStatus,
-            Owner owner, Property property) {
+            String description, Owner owner, Property property) {
         Repair repair = new Repair();
         repair.setRepairType(repairType);
         repair.setShortDescription(shortDescription);
@@ -34,6 +33,61 @@ public class RepairServiceImpl implements RepairService {
         repair.setProperty(property);
         repairRepository.save(repair);
         return repair;
+    }
+
+    @Override
+    public void updGeneral(Long id, RepairType repairType, String shortDescription,
+            String description) {
+        Optional<Repair> repair = repairRepository.findById(id);
+        Repair repairFound = repair.get();
+        if (repairType != null) {
+            repairFound.setRepairType(repairType);
+        }
+        if (shortDescription != null) {
+            repairFound.setShortDescription(shortDescription);
+        }
+        if (description != null) {
+            repairFound.setDescription(description);
+        }
+        repairFound.setSubmissionDate(LocalDateTime.now());
+        repairFound.setRepairStatus(RepairStatus.PENDING);
+        repairRepository.save(repairFound);
+    }
+
+    @Override
+    public void updCostDates(Long id, BigDecimal proposedCost, LocalDateTime proposedStartDate, LocalDateTime proposedEndDateTime) {
+        Optional<Repair> repair = repairRepository.findById(id);
+        Repair repairFound = repair.get();
+        if (proposedCost != null) {
+            repairFound.setProposedCost(proposedCost);
+        }
+        repairFound.setProposedStartDate(proposedStartDate);
+        repairFound.setProposedEndDate(proposedEndDateTime);
+        repairRepository.save(repairFound);
+    }
+
+    @Override
+    public void updAcceptance(Long id, int response) {
+        Optional<Repair> repair = repairRepository.findById(id);
+        Repair repairFound = repair.get();
+        if (response == 1) {
+            repairFound.setAcceptanceStatus(Boolean.TRUE);
+            repairFound.setRepairStatus(RepairStatus.INPROGRESS);
+            repairFound.setActualStartDate(LocalDateTime.now());
+        } else {
+            repairFound.setAcceptanceStatus(Boolean.FALSE);
+            repairFound.setRepairStatus(RepairStatus.DECLINED);
+        }
+        repairRepository.save(repairFound);
+    }
+
+    @Override
+    public void updComplete(Long id) {
+        Optional<Repair> repair = repairRepository.findById(id);
+        Repair repairFound = repair.get();
+        repairFound.setActualEndDate(LocalDateTime.now());
+        repairFound.setRepairStatus(RepairStatus.COMPLETE);
+        repairRepository.save(repairFound);
     }
 
     @Override
@@ -56,7 +110,7 @@ public class RepairServiceImpl implements RepairService {
     public Repair findRepairByDate() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
     @Override
     public Repair deleteUserSafely(Long id) {
         //Repair repair2 = null ;
@@ -67,7 +121,5 @@ public class RepairServiceImpl implements RepairService {
         repairRepository.save(repair2);
         return repair2;
     }
-
-
 
 }
