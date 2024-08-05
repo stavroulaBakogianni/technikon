9c6b8aa2653e602476a580c7974d5ef158105eb4
Stavroula Bakogianni <stauroulabak@gmail.com>
2024-08-06 01:32:49 +0300

    Run csv importer methods on app start

--- src/main/java/gr/technico/technikon/Technikon.java
+++ src/main/java/gr/technico/technikon/Technikon.java
@@ -4,6 +4,8 @@ import gr.technico.technikon.services.OwnerService;
 import gr.technico.technikon.repositories.OwnerRepository;
 import gr.technico.technikon.jpa.JpaUtil;
 import gr.technico.technikon.services.importfiles.OwnerCSVImporter;
+import gr.technico.technikon.services.importfiles.PropertyCSVImporter;
+import gr.technico.technikon.services.importfiles.RepairCSVImporter;
 import gr.technico.technikon.ui.MainMenuUI;
 import java.io.IOException;
 
@@ -13,6 +15,10 @@ public class Technikon {
 
         OwnerCSVImporter ownerCSVImporter = new OwnerCSVImporter();
         ownerCSVImporter.importFile("csv_files/owners.csv");
+        PropertyCSVImporter propertyCSVImporter = new PropertyCSVImporter();
+        propertyCSVImporter.importFile("csv_files/properties.csv");
+        RepairCSVImporter repairCSVImporter = new RepairCSVImporter();
+        repairCSVImporter.importFile("csv_files/repairs.csv");
 
         OwnerRepository ownerRepository = new OwnerRepository(JpaUtil.getEntityManager());
         OwnerService ownerService = new OwnerService(ownerRepository);
